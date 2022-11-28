501b045a5c5b3fc8ffdbd1c6090e8439151f60f5
mjdemars <maddalendemars@gmail.com>
2022-11-27 23:42:42 -0500

    Adding more interaction and sound

--- 2730 Final Project/Assets/Scripts/noteAppear.cs
+++ 2730 Final Project/Assets/Scripts/noteAppear.cs
@@ -6,26 +6,33 @@ using UnityEngine.UI;
 public class noteAppear : MonoBehaviour
 {
     [SerializeField]
-    private Image _ingredientsImage;
+    private Image _noteImage;
 
     GameObject mainCamera;
+    public GameObject noteObject;
 
     bool pickedUp;
-    bool recipePickedup;
+    public bool twoAudios;
 
-    public AudioSource recipe;
+    public AudioSource voiceline;
+    public AudioSource voiceline2;
+
+    public float detectionRange;
+    public bool closeEnough;
+    public Transform player;
 
     // Start is called before the first frame update
     void Start()
     {
         mainCamera = GameObject.FindWithTag("MainCamera");
         pickedUp = false;
-        recipePickedup = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        closeEnough = false;
+
         if(pickedUp) {
             drop();
         } else {
@@ -35,43 +42,36 @@ public class noteAppear : MonoBehaviour
 
     void pickup() {
         if(Input.GetKeyDown (KeyCode.E)) {
-            if (recipePickedup == false) {
-                recipe.Play();
+            if (Vector3.Distance(player.position, transform.position) <= detectionRange) {
+
+                closeEnough = true;
+
+                voiceline.Play();
+                if (twoAudios == true && voiceline.isPlaying == false) {
+                    voiceline2.Play();
+                }
 
                 int x = Screen.width / 2;
                 int y = Screen.height / 2;
 
                 Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(x,y));
                 RaycastHit hit;
+
                 if(Physics.Raycast(ray, out hit)) {
-                    _ingredientsImage.enabled = true;
+                    _noteImage.enabled = true;
                     pickedUp = true;
                 }
-                recipePickedup = true;
             }
         }
     }
 
     void drop() {
         if(Input.GetKeyDown (KeyCode.E)) {
-            _ingredientsImage.enabled = false;
+            _noteImage.enabled = false;
+
+            noteObject.SetActive(false);
             pickedUp = false;
         }
     }
-
-
-        // void OnTriggerEnter(Collider other) {
-    //     if (other.CompareTag("Player")) {
-    //         if(Input.GetKeyDown (KeyCode.E)) {
-    //             _ingredientsImage.enabled = true;
-    //         }
-    //     }
-    // }
-
-    // void OnTriggerExit(Collider other) {
-    //      if (other.CompareTag("Player")) {
-    //         _ingredientsImage.enabled = false;
-    //     }
-    // }
 
 }
